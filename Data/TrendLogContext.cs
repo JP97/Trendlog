4c29aab2d54055156950867b47622066ed00f926
Julian <juli11c0@edu.eal.dk>
2019-10-23 10:50:25 +0200

    created TrendLogContext

---
+++ Data/TrendLogContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trendlogg.Models;
+
+namespace Trendlogg.Data
+{
+    public class TrendLogContext : DbContext
+    {
+        public DbSet<Channel> Channels { get; set; }
+        public DbSet<Feed> Feeds { get; set; }
+        public DbSet<Point> Points { get; set; }
+    }
+}
